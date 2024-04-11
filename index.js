7cf3ab2447ab7d031e686bcfbb96f4aa9bedb911
tonniegeetz <tonniegeetz@gmail.com>
2024-04-11 22:43:04 +0300

    Pracice on for loops and while loops

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solutions in this file
+
+function writeCards(array) {
+  let cards = [];
+
+  for (let i = 0; i < array.length; i++) {
+    cards.push(`Thank you, ${array[i]}, for the wonderful surprise gift!`);
+  }
+
+  return cards;
+}
+
+function countDown(num) {
+  while (num > 0) {
+    console.log(num);
+    num--;
+  }
+  console.log(num);
+}
